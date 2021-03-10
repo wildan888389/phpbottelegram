601b2ec69302497470619095a2f479caa4a5c84c
wildan888389 <80402846+wildan888389@users.noreply.github.com>
2021-03-10 22:03:03 +0800

    Update bot-api-config.php

--- bot-api-config.php
+++ bot-api-config.php
@@ -4,7 +4,8 @@ if (!defined('HS')) {
     die('Tidak boleh diakses langsung.');
 }
 
-$token = '';
+$token = '1547884697:AAGH88IHpK8bvN93RtnVCHudw3ZlgPpljxw';
 $debug = true;
 
 $chatid = '213567634';
+
